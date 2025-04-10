9bb6ab2e9b8f955324db934bc2806e4b645e6daf
“강예진” <yeajink@krsys.net>
2025-04-10 15:32:16 +0900

    [25.04.10] 작업내역 임시 저장장

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="center">
+    <button @click="router.push('/payment')">결제 페이지로 이동</button>
+    <button @click="router.push('/test')">test 페이지로 이동</button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
+<style>
+.center {
+   text-align: center;
+}
+</style>
